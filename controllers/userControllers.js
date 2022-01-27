26986240a278097e55f1805339aacae5acac0360
Ayush Garg <ayush181000@gmail.com>
2022-01-27 11:09:42 +0530

    completed user functions

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,7 +3,7 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc        Register new user
-// @route       GET /users
+// @route       POST /user/register
 // @access      Public
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -30,6 +30,8 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      address: user.address,
+      phoneNumber: user.phoneNumber,
       token: generateToken(user._id),
     });
   } else {
@@ -38,8 +40,8 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc        Auth user and auth token
-// @route       POST /users/login
+// @desc        Login user
+// @route       POST /user/login
 // @access      Public
 
 const loginUser = asyncHandler(async (req, res) => {
@@ -53,6 +55,8 @@ const loginUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      address: user.address,
+      phoneNumber: user.phoneNumber,
       token: generateToken(user._id),
     });
   } else {
@@ -61,4 +65,105 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser };
+// @desc        Get user profile
+// @route       GET /users/profile
+// @access      Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc        Update user profile
+// @route       PUT /users/profile
+// @access      Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.user._id, req.body);
+
+  if (user) {
+    res.status(204).json({
+      user,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc        Get all users
+// @route       GET /users
+// @access      Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+// @desc        Delete a user
+// @route       DELETE /users/:id
+// @access      Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: "User Removed" });
+  } else {
+    res.status(400);
+    throw new Error("No such user found");
+  }
+});
+
+// @desc        Get user by ID
+// @route       GET /users/:id
+// @access      Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400);
+    throw new Error("No such user found");
+  }
+});
+
+// @desc        Update user
+// @route       PUT /users/:id
+// @access      Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,29 @@
 import express from "express";
 const router = express.Router();
 
-import { loginUser, registerUser } from "../controllers/userControllers.js";
+import {
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from "../controllers/userControllers.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
+router.route("/").get(protect, admin, getUsers);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
