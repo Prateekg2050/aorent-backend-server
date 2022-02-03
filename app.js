794b959d5152be0eee48dbfdb91e3c9b3672e2fb
Ayush Garg <ayush181000@gmail.com>
2022-02-03 23:32:30 +0530

    cors added

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', userRoutes);
 app.use('/product', productRoutes);
